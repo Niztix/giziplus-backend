93fb6ca627aec6632968b662d73f4ac91b941f83
aldipeha@gmail.com <aldighifari0@gmail.com>
2023-06-18 23:44:03 +0700

    updating cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require("./config/database").connect();
 const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
